1c2c168eacb489e941fc7f2b535623b7d8788560
boatzhang <andyboat1009@gmail.com>
2023-03-19 21:12:27 +0800

    feat: add more declarative command line prase

--- index.ts
+++ index.ts
@@ -2,8 +2,9 @@ import createServer from "./create-server";
 import { exec } from "node:child_process";
 import os from "node:os";
 import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
-const startServe = async (filePath: string) => {
+const startServe = async (filePath: string, port: number) => {
   const localIpInternal: string = (() => {
     const interfaces = os.networkInterfaces();
     for (const iface in interfaces) {
@@ -19,24 +20,47 @@ const startServe = async (filePath: string) => {
   })();
 
   // (async () => {
-  const port = 8080;
   const createRes = await createServer(filePath, port);
   if (createRes) {
     const targetUrl = `http://${localIpInternal}:${port}/awesome.mp4`;
-    console.info(">>>targetUrl", targetUrl);
-    exec(`open -a Safari ${targetUrl}`);
+    const command = `open -a /Applications/Safari.app ${targetUrl}`;
+    console.info("local url: ", targetUrl);
+    console.info(
+      `Awesome Touping should open a Safari tab with the url above. If it failed, you can open it manually by :\n${command}`
+    );
+    exec(`open -a /Applications/Safari.app ${targetUrl}`);
   }
   // })();
 };
 
 const argv = yargs
+  .scriptName("awesome-touping")
   .command(
-    "serve [file]",
-    "serve local video file with an accessible url for AirPlay across devices"
+    "serve <path>",
+    "serve local video file with an accessible url in local area network on MacOS. Mp4 only now.",
+    (yargs) => {
+      yargs.positional("path", {
+        describe: "local video file path",
+        type: "string",
+      });
+    }
   )
-  .help()
+  .option("port", {
+    alias: "p",
+    type: "number",
+    default: 9527,
+    description: "Local port to serve from",
+  })
+  .demandCommand(1, "You need at least one command before moving on")
+  .example([
+    ["$0 serve example.mp4", "Serve the example.mp4"],
+    [
+      "$0 serve example.mp4 -p 3000",
+      "Serve the example.mp4 from local port 3000",
+    ],
+  ])
   .alias("help", "h").argv;
 
-const { file: filePath } = argv as any;
-console.info(filePath);
-startServe(filePath);
+const { path: filePath, port } = argv as any;
+// console.info({ filePath, port });
+startServe(filePath, port);
